32ce8a5883a222fa7ef445b22a8dc90925ade3ca
Marios Tsolekas <marios.tsolekas@gmail.com>
2018-02-19 03:14:02 +0200

    Migrated from intltool to gettext(more changes may need to be done)

--- src/main.c
+++ src/main.c
@@ -23,14 +23,15 @@
 #include <gtk/gtk.h>
 #include <glib/gi18n.h>
 #include <locale.h>
+#include <libintl.h>
 #include "fsearch.h"
 
 int
 main (int argc, char *argv[])
 {
     setlocale (LC_ALL, "");
-    bindtextdomain (GETTEXT_PACKAGE, LOCALEDIR);
-    bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
-    textdomain (GETTEXT_PACKAGE);
+    bindtextdomain (PACKAGE, LOCALEDIR);
+    bind_textdomain_codeset (PACKAGE, "UTF-8");
+    textdomain (PACKAGE);
     return g_application_run (G_APPLICATION (fsearch_application_new ()), argc, argv);
 }
